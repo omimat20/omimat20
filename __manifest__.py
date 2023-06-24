5fb82d3d3e6540d4f151c9fd6c92e46e4cd8724f
Omima Talal <137509428+omimat20@users.noreply.github.com>
2023-06-24 17:05:01 +0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Car Store',
+    'category': 'Tools',
+    'summary': 'this',
+    'description': "",
+    'version': '1.0',
+    'depends': ['base'],
+    'data': ['views\car_store_views.xml',
+             'views\companies_views.xml',
+             'security\ir.model.access.csv'
+             ],
+    'demo':[],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
